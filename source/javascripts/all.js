6b8cf893d06a2d6661563a199893dfd2227d3426
pam- <pamela.assogba@voxmedia.com>
2017-03-12 17:30:18 -0400

    adding new page

---
+++ source/javascripts/all.js
@@ -0,0 +1,17 @@
+//= require bootstrap
+//= require_tree .
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+});
