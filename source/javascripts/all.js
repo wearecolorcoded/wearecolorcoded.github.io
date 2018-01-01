9216d827be5381764f08531c075c3725a34a2b3d
pam- <pamela.assogba@voxmedia.com>
2018-01-01 14:58:38 -0500

    RESTYLE

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,3 @@
-//= require bootstrap
-//= require_tree .
-
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -17,26 +14,8 @@ $(function() {
 });
 
 $(document).ready(function() {
-
-  var eventDetails = $('body').find('[data-event-nav]');
-  eventDetails.on('click', function() {
-
-    var $parent = $(this).parents('[data-event-details]');
-
-    $parent.find('[data-event-show]').toggleClass('showing');
-    $parent.find('[data-event-body]').toggleClass('showing');
-  })
-
-  $('.m-content__index-carousel').slick({
-    accessibility: true,
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-  });
-
   $('.nav__hamburger-menu').on('click', function(){
+    $(this).toggleClass('open');
     $('.m-content').toggleClass('active');
   });
-
 })
